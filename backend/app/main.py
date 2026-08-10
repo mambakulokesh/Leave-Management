42b8bf04e593ae7e6e157c04c86c14502d080d8f
Lokesh Mambaku <mambakulokesh4756@gmail.com>
2026-08-10 20:07:30 +0530

    Update CORS configuration for frontend prod.

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://leave-management-fe-a2l1.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
